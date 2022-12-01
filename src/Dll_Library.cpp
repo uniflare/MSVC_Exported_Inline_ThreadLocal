98637ed95c7e1a65c74b771799bf24e6f61ac6f3
uniflare <uniflare@gmail.com>
2022-12-01 15:25:47 +0100

    Add thread_local specifier to showcase error

--- src/Dll_Library.cpp
+++ src/Dll_Library.cpp
@@ -2,6 +2,6 @@
 
 void ExportedClass::TestFunc()
 {
-	static ThreadLocalData data; // <- If this item is exported with latest v143 toolset, it will create a LNK1161: invalid export specification
+	static thread_local ThreadLocalData data; // <- If this item is exported with latest v143 toolset, it will create a LNK1161: invalid export specification
 	data.i += 1;
 }
